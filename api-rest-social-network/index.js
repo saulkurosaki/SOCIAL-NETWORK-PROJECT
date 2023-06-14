610a92e7da1430a6c72465dd651b41c4d6b8d060
Saul Paredes <saulkurosaki13@gmail.com>
2023-06-14 00:02:33 -0600

    Index.js complete Refactor Done

--- api-rest-social-network/index.js
+++ api-rest-social-network/index.js
@@ -1,4 +1,6 @@
 const connection = require('./database/connection');
+const express = require('express');
+const cors = require('cors');
 
 //Mensaje de Bienvenida
 console.log('Api Node Inicializada');
@@ -6,12 +8,27 @@ console.log('Api Node Inicializada');
 //Conexión a DataBase
 connection();
 
-//Crear servidor Node
+// Crear servidor node
+const app = express();
+const puerto = 3000;
 
-//Configurar Cors
+// Configurar cors
+app.use(cors());
 
-//Convertir los datos del body a objetos JS
+// Convertir los datos del body a objetos js
+app.use(express.json());
+app.use(express.urlencoded({extended: true})); 
 
-//Cargar config rutas
+// Cargar conf rutas
+const UserRoutes = require("./routes/user");
+const PublicationRoutes = require("./routes/publication");
+const FollowRoutes = require("./routes/follow");
 
-//Poner servidor a escuchar peticiones http
+app.use("/api/user", UserRoutes);
+app.use("/api/publication", PublicationRoutes);
+app.use("/api/follow", FollowRoutes);
+
+// Poner servidor a escuchar peticiones http
+app.listen(puerto, () => {
+    console.log("Servidor de node corriendo en el puerto: ", puerto);
+});
