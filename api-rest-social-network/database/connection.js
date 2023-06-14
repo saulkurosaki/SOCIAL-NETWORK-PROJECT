291af3c2b9be57fae3ad912256c5d86ffe064eed
Saul Paredes <saulkurosaki13@gmail.com>
2023-06-13 23:53:15 -0600

    Connection to the DataBase Done

---
+++ api-rest-social-network/database/connection.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const connection = async () => {
+
+    try {
+        await mongoose.connect('mongodb://127.0.0.1:27017/mi_redsocial');
+
+        console.log('Conectado correctamente a la Base de Datos ');
+
+    } catch (error) {
+        console.log(error);
+        throw new Error('No se ha podido conectar a la base de datos');
+    }
+
+};
+
+module.exports = connection;
---
+++ api-rest-social-network/index.js
@@ -0,0 +1,17 @@
+const connection = require('./database/connection');
+
+//Mensaje de Bienvenida
+console.log('Api Node Inicializada');
+
+//Conexión a DataBase
+connection();
+
+//Crear servidor Node
+
+//Configurar Cors
+
+//Convertir los datos del body a objetos JS
+
+//Cargar config rutas
+
+//Poner servidor a escuchar peticiones http
