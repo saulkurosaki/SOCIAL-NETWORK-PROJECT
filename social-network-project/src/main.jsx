764cde3c46e2c315b8dbc8af5323b2041ab6c28a
Saul Paredes <saulkurosaki13@gmail.com>
2023-06-16 16:27:22 -0600

    Assets installed and General NavBar Done

--- social-network-project/src/main.jsx
+++ social-network-project/src/main.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+
+//Importar Assets (recursos)
+import './assets/fonts/fontawesome-free-6.1.2-web/css/all.css';
+import './assets/css/normalize.css';
+import './assets/css/styles.css';
+import './assets/css/responsive.css';
+
+//Arrancar App de React
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />
 )
