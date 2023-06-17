1689d1e15ea9aad8d72f3c4e9fc05e3f30c38776
Saul Paredes <saulkurosaki13@gmail.com>
2023-06-17 02:16:17 -0600

    Login.jsx Form Structure Done

--- social-network-project/src/components/user/Login.jsx
+++ social-network-project/src/components/user/Login.jsx
@@ -9,6 +9,22 @@ export const Login = () => {
 
         <div className='content_posts'>
 
+          <form className='form-login'>
+
+            <div className='form-group'>
+              <label htmlFor='email'>Email</label>
+              <input type='email' name='email'/>
+            </div>
+
+            <div className='form-group'>
+              <label htmlFor='password'>Contraseña</label>
+              <input type='password' name='password'/>
+            </div>
+
+            <input type='submit' value='Identificate' className='btn btn-success'/>
+
+          </form>
+
         </div>
     </>
   )
