f8a7b5cffc6b7d863a4a49bfdc6ec94d21989cbe
Saul Paredes <saulkurosaki13@gmail.com>
2023-06-17 02:37:21 -0600

    Login Page with LocalStrg and JWT Completed

--- social-network-project/src/components/user/Login.jsx
+++ social-network-project/src/components/user/Login.jsx
@@ -1,31 +1,75 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useForm } from '../../hooks/useForm'
+import { Global } from '../../helpers/Global';
 
 export const Login = () => {
+
+  const { form, changed } = useForm({});
+  const [saved, setSaved] = useState('not_sended');
+
+  const loginUser = async (e) => {
+    e.preventDefault();
+
+    //Datos del formulario
+    const userToLogin = form;
+
+    //Peticion al Backend
+    const request = await fetch(Global.url + 'user/login', {
+      method: "POST",
+      body: JSON.stringify(userToLogin),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    });
+
+    const data = await request.json();
+
+    if(data.status == 'success'){
+
+      //Persistir los datos en el navegador
+      localStorage.setItem('token', data.token);
+      localStorage.setItem('user', JSON.stringify(data.user));
+
+      setSaved('login');
+    }else{
+      setSaved('error');
+    };
+
+
+  };
+
   return (
     <>
-        <header className="content__header content__header--public">
-            <h1 className="content__title">Login</h1>
-        </header>
+      <header className="content__header content__header--public">
+        <h1 className="content__title">Login</h1>
+      </header>
 
-        <div className='content_posts'>
+      <div className='content_posts'>
 
-          <form className='form-login'>
+        {saved == 'login'
+          ? <strong className='alert alert-success'>Usuario identificado correctamente</strong>
+          : null}
+        {saved == 'error'
+          ? <strong className='alert alert-danger'>Usuario no identificado</strong>
+          : null}
 
-            <div className='form-group'>
-              <label htmlFor='email'>Email</label>
-              <input type='email' name='email'/>
-            </div>
+        <form className='form-login' onSubmit={loginUser}>
 
-            <div className='form-group'>
-              <label htmlFor='password'>Contraseña</label>
-              <input type='password' name='password'/>
-            </div>
+          <div className='form-group'>
+            <label htmlFor='email'>Email</label>
+            <input type='email' name='email' onChange={changed} />
+          </div>
 
-            <input type='submit' value='Identificate' className='btn btn-success'/>
+          <div className='form-group'>
+            <label htmlFor='password'>Contraseña</label>
+            <input type='password' name='password' onChange={changed} />
+          </div>
 
-          </form>
+          <input type='submit' value='Identificate' className='btn btn-success' />
 
-        </div>
+        </form>
+
+      </div>
     </>
   )
 }
