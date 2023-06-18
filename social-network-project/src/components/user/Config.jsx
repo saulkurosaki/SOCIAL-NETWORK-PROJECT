5ffbec551a3e73e8aac6a44d0b9d21b8a8eb5b24
Saul Paredes <saulkurosaki13@gmail.com>
2023-06-18 16:12:55 -0600

    Config Form structure Done

--- social-network-project/src/components/user/Config.jsx
+++ social-network-project/src/components/user/Config.jsx
@@ -1,7 +1,69 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const Config = () => {
-  return (
-    <div>Config</div>
-  )
+
+    const [saved, setSaved] = useState('not_saved');
+
+    const updateUser = (e) => {
+        e.preventDefault();
+
+
+    };
+
+    return (
+        <>
+            <header className="content__header content__header--public">
+                <h1 className="content__title">Configuración</h1>
+            </header>
+
+            <div className='content_posts'>
+                {saved == 'saved'
+                    ? <strong className='alert alert-success'>Usuario registrado correctamente</strong>
+                    : null}
+                {saved == 'error'
+                    ? <strong className='alert alert-danger'>Usuario no registrado</strong>
+                    : null}
+
+                <form className='config-form' onSubmit={updateUser}>
+
+                    <div className='form-group'>
+                        <label htmlFor='name'>Nombre</label>
+                        <input type='text' name='name' />
+                    </div>
+
+                    <div className='form-group'>
+                        <label htmlFor='surname'>Apellidos</label>
+                        <input type='text' name='surname' />
+                    </div>
+
+                    <div className='form-group'>
+                        <label htmlFor='nick'>Nickname</label>
+                        <input type='text' name='nick' />
+                    </div>
+
+                    <div className='form-group'>
+                        <label htmlFor='email'>Correo Electrónico</label>
+                        <input type='email' name='email' />
+                    </div>
+
+                    <div className='form-group'>
+                        <label htmlFor='password'>Contraseña</label>
+                        <input type='password' name='password' />
+                    </div>
+
+                    <div className='form-group'>
+                        <label htmlFor='file0'>Avatar</label>
+                        <div className='avatar'>
+                            {/* Mostrar Imagen */}
+                        </div>
+                        <input type="file" name="file0" id="file" />
+                    </div>
+                    <br/>
+
+                    <input type='submit' value='Regístrate' className='btn btn-success' />
+
+                </form>
+            </div>
+        </>
+    )
 }
