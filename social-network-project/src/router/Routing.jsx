c52f20669697523cf743f1588ed55743cfc184f9
Saul Paredes <saulkurosaki13@gmail.com>
2023-06-18 15:58:51 -0600

    Private NavBar User Data

--- social-network-project/src/router/Routing.jsx
+++ social-network-project/src/router/Routing.jsx
@@ -7,6 +7,8 @@ import { Register } from '../components/user/Register';
 import { Feed } from '../components/publication/Feed';
 import { AuthProvider } from '../context/AuthProvider';
 import { Logout } from '../components/user/Logout';
+import { People } from '../components/user/People';
+import { Config } from '../components/user/Config';
 
 export const Routing = () => {
     return (
@@ -28,6 +30,8 @@ export const Routing = () => {
                         <Route index element={<Feed />} />
                         <Route path='feed' element={<Feed />} />
                         <Route path='logout' element={<Logout />}/>
+                        <Route path='gente' element={<People />}/>
+                        <Route path='ajustes' element={<Config />}/>
                     </Route>
 
                     //Ruta 404
