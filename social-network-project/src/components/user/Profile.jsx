c140167d9f74ac6a06abb2bae89da9d1fc425e66
Saul Paredes <saulkurosaki13@gmail.com>
2023-06-20 01:35:41 -0600

    Profile.jsx and PublicationList.jsx Refactor Done

--- social-network-project/src/components/user/Profile.jsx
+++ social-network-project/src/components/user/Profile.jsx
@@ -4,6 +4,7 @@ import { GetProfile } from '../../helpers/GetProfile';
 import { Link, useParams } from 'react-router-dom';
 import { Global } from '../../helpers/Global';
 import useAuth from '../../hooks/useAuth';
+import { PublicationList } from '../publication/PublicationList';
 
 export const Profile = () => {
 
@@ -52,8 +53,6 @@ export const Profile = () => {
         if (data.following) {
             setCounters(data);
         };
-
-        console.log(user);
 
     };
 
@@ -118,7 +117,7 @@ export const Profile = () => {
                 newPublications = [...publications, ...data.publications];
             }
 
-            if(newProfile){
+            if (newProfile) {
                 newPublications = data.publications;
                 setMore(true);
                 setPage(1);
@@ -130,34 +129,11 @@ export const Profile = () => {
                 setMore(false);
             };
 
-            if(data.pages <= 1){
+            if (data.pages <= 1) {
                 setMore(false);
             }
 
         }
-    };
-
-    const nextPage = () => {
-        let next = page + 1;
-        setPage(next);
-        getPublications(next);
-    };
-
-    const deletePublication = async (publicationId) => {
-        const request = await fetch(Global.url + 'publication/remove/' + publicationId, {
-            method: 'DELETE',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': localStorage.getItem('token'),
-            },
-        });
-
-        const data = await request.json();
-        
-        setPage(1);
-        setMore(true);
-        getPublications(1, true);
-
     };
 
     return (
@@ -222,63 +198,14 @@ export const Profile = () => {
 
             </header>
 
-
-            <div className="content__posts">
-
-                {publications.map(publication => {
-
-                    return (
-
-                        <article className="posts__post" key={publication._id}>
-
-                            <div className="post__container">
-
-                                <div className="post__image-user">
-                                    <Link to={'/social/perfil/' + publication.user._id} className="post__image-link">
-                                        {publication.user.image != 'default.png' && <img src={Global.url + 'user/avatar/' + publication.user.image} className="post__user-image" alt="Foto de perfil" />}
-                                        {publication.user.image == 'default.png' && <img src={avatar} className="post__user-image" alt="Foto de perfil" />}
-                                    </Link>
-                                </div>
-
-                                <div className="post__body">
-
-                                    <div className="post__user-info">
-                                        <a href="#" className="user-info__name">{publication.user.name}{publication.user.surname}</a>
-                                        <span className="user-info__divider"> | </span>
-                                        <a href="#" className="user-info__create-date">{publication.created_at}</a>
-                                    </div>
-
-                                    <h4 className="post__content">{publication.text}</h4>
-
-                                </div>
-
-                            </div>
-
-                            {auth._id == publication.user._id &&
-                                <div className="post__buttons">
-
-                                    <button onClick={() => deletePublication(publication._id)} className="post__button">
-                                        <i className="fa-solid fa-trash-can"></i>
-                                    </button>
-
-                                </div>
-                            }
-
-                        </article>
-
-                    );
-
-                })}
-
-            </div>
-
-            {more &&
-                <div className="content__container-btn" onClick={nextPage}>
-                    <button className="content__btn-more-post">
-                        Ver mas publicaciones
-                    </button>
-                </div>
-            }
+            <PublicationList 
+                publications={publications}
+                getPublications={getPublications}
+                page={page}
+                setPage={setPage}
+                more={more}
+                setMore={setMore}
+            />
 
             <br />
         </>
